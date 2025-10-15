426302a69b8d8e689203cbd8191f1331982e39ed
ESouza7 <saigagamer666@gmail.com>
2025-10-15 20:18:26 -0300

    Update imagens.js

--- imagens.js
+++ imagens.js
@@ -8,7 +8,7 @@
   { "url": "https://i.imgur.com/dnqVhmq.png", "chunk": [756, 1162], "coords": [885, 661] },
   { "url": "https://i.imgur.com/fqeR2Vo.png", "chunk": [756, 1161], "coords": [602, 942] },
   { "url": "https://i.imgur.com/A6oUTlJ.png", "chunk": [757, 1162], "coords": [480, 553] },
-  { "url": "https://i.ibb.co/CKcP7Dmk/converted-template-base-principal-e-secundaria.png", "chunk": [754, 1156], "coords": [195, 465] },
+  { "url": "https://i.ibb.co/1Y4bjw6x/converted-template-base-principal-e-secundaria.png", "chunk": [754, 1156], "coords": [195, 465] },
   { "url": "https://i.imgur.com/83ZQmU6.png", "chunk": [778, 1155], "coords": [137, 323] },
   { "url": "https://i.imgur.com/G72vqiE.png", "chunk": [1818, 796], "coords": [206, 280] },
   { "url": "https://i.imgur.com/BXdnCko.png", "chunk": [756, 1160], "coords": [989, 780] },
